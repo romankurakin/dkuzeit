bff54f8ddf05fd5434d063653dd9017f70b731ed
Roman Kurakin <roman@kurakindev.com>
2026-03-14 14:14:28 +0500

    chore: up tracesSampleRate to half of requests

--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -3,7 +3,7 @@ import type { TransactionEvent } from '@sentry/core';
 
 export const sentryConfig = {
 	dsn: 'https://2b9222adeea60d9dbaef826f52937788@o4510862703722496.ingest.us.sentry.io/4510862792589312',
-	tracesSampleRate: 0.25,
+	tracesSampleRate: 0.5,
 	beforeSendTransaction: (event: TransactionEvent) => {
 		const status = event.contexts?.trace?.data?.['http.response.status_code'];
 		if (status === 404 || status === '404') return null;
